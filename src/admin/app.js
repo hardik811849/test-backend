82cc3d081a46805735f1a9d56c963f310fe4d423
hardik Gajera <hardik@gfuturetech.com>
2024-06-19 13:18:01 +0530

    first

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,11 +1,17 @@
-import fav from "./extensions/favicon.ico";
+import fav from "./extensions/Frame 1618873746.png";
 
 const config = {
   // Replace the favicon
+
   head: {
     favicon: fav,
   },
-
+  auth: {
+    logo: fav, // Replace with the path to your logo
+  },
+  menu: {
+    logo: fav, // Replace with the path to your logo
+  },
   // Extend the translations
   translations: {
     en: {
